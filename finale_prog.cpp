932dd0094c4695947d889aa700efe065f9e764f0
Kiryanov Ilya <kiryanov.ilya@bk.ru>
2022-11-07 15:27:42 +0300

    cout and fill fix

--- finale_prog.cpp
+++ finale_prog.cpp
@@ -4,11 +4,18 @@
 using namespace std;
 
 /**
- * \brief Get sizw if the array
+ * \brief Get size if the array
  * \return Size_t size of array
  * \exception Function throws exception invalid_argument if array has invalid size
  */
 size_t get_size();
+
+/**
+ * \brief Fills the array with pseudo-random values
+ * \param Accepts a pointer to array
+ * \param Accepts size of the array
+ */
+void fill_array(unsigned char*& values, const size_t size);
 
 /**
 * \brief Print array in size_t and char types
@@ -32,7 +39,7 @@ size_t algo(unsigned char* values, const size_t size);
 void remove_array(unsigned char*& values);
 
 int main() {
-    size_t tmp_size; //To catch invalid array size
+    size_t tmp_size;
     try {
         tmp_size = get_size();
     } catch (const invalid_argument& e) {
@@ -43,18 +50,11 @@ int main() {
     const size_t size = tmp_size;
 
     unsigned char* values = new unsigned char[size];
-    const size_t lower = 0;
-    const size_t upper = 255;
-    std::random_device rd;
-    mt19937 gen(rd());
-    std::uniform_int_distribution<> dist(lower, upper);
-    for (size_t i = 0; i < size; i++) {
-        values[i] = dist(gen);
-    }
+    fill_array(values, size);
     print_array(values, size);
 
     size_t result = algo(values, size);
-    cout << "Result in char is" << (char)result << "\nResult in integer value is " << result << endl;
+    cout << "\nResult in char is " << static_cast<char>(result) << "\nResult in integer value is " << result << endl;
     remove_array(values);
     return 0;
 }
@@ -68,6 +68,17 @@ size_t get_size() {
         throw invalid_argument("The array size must be greater than 0");
     }
     return static_cast<size_t>(size);
+}
+
+void fill_array(unsigned char*& values, const size_t size) {
+    const size_t lower = 0;
+    const size_t upper = 255;
+    std::random_device rd;
+    mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(lower, upper);
+    for (size_t i = 0; i < size; i++) {
+        values[i] = dist(gen);
+    }
 }
 
 void print_array(unsigned char* values, const size_t size) {
@@ -87,18 +98,18 @@ void print_array(unsigned char* values, const size_t size) {
 }
 
 size_t algo(unsigned char* values, const size_t size) {
-    cout << "\n\nThe algorithm has started\n";
     if (values == nullptr) throw invalid_argument("The array size must be greater than 0\n");
 
     size_t res = 0;
     for (size_t i = 0; i < size; i++) {
         res += values[i];
     }
-    cout << "The algorithm has finished\n\n";
     return res / size;
 }
 
 void remove_array(unsigned char*& values) {
-    delete[] values;
-    values = nullptr;
+    if (values != nullptr) {
+        delete[] values;
+        values = nullptr;
+    }
 }
